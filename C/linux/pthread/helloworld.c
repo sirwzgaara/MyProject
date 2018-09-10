2fdc1b655e27ef1e692d365784c396cd15adc19b
e450c-linux <1819644289@qq.com>
2018-09-10 19:55:27 +0800

    modified:   linux/pthread/a.out 	modified:   linux/pthread/helloworld.c

--- C/linux/pthread/helloworld.c
+++ C/linux/pthread/helloworld.c
@@ -4,16 +4,61 @@
 #include <fcntl.h>
 #include <string.h>
 #include <sys/types.h>
+#include <pthread.h>
+#include <signal.h>
+#include <time.h>
+
+pthread_t tid;
+sigset_t set;
+
+void myfunc()
+{
+    printf("hello\n");
+}
+
+static void *mythread(void *p)
+{
+    int signum;
+    while (1)
+    {
+        sigwait(&set, &signum);
+        if (SIGUSR1 == signum)
+            myfunc();
+        if (SIGUSR2 == signum)
+        {
+            printf("i will sleep 2 second and exit\n");
+            sleep(2);
+            break;
+        }
+    }
+}
 
 void main()
 {
-    int fd;
-    fd = open("/tmp/server_read", O_WRONLY);
-    if (fd < 0)
+    char tmp;
+    void *status;
+    sigemptyset(&set);
+    sigaddset(&set, SIGUSR1);
+    sigaddset(&set, SIGUSR2);
+    sigprocmask(SIG_SETMASK, &set, NULL);
+    pthread_create(&tid, NULL, mythread, NULL);
+
+    while (1)
     {
-        printf("failed\n");
-        return;
+        printf(":");
+        scanf("%c", &tmp);
+        if ('a' == tmp)
+        {
+            pthread_kill(tid, SIGUSR1);
+        }
+        else if ('q' == tmp)
+        {
+            pthread_kill(tid, SIGUSR2);
+            pthread_join(tid, &status);
+            printf("finish");
+            break;
+        }
+        else
+            continue;
     }
-    printf("open ok\n");
-    close(fd);
 }
